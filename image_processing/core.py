9a86ade5d68b4a507684baf98cd5627b17cd7cfd
Surya Suhaas <mssuhaas@gmail.com>
2024-01-28 11:05:00 +0530

    Changed the initial_quality variable to default function parameter

--- image_processing/core.py
+++ image_processing/core.py
@@ -1,11 +1,11 @@
 from PIL import Image
 import os
 
-def resize_and_compress(input_path, output_path):
+
+def resize_and_compress(input_path, output_path, initial_quality=90):
     original_image = Image.open(input_path)
     max_size_bytes = 100 * 1024  # 100 KB
     original_image.thumbnail((800, 800))
-    initial_quality = 90
     temp_path = "temp_resized_image.jpg"
     original_image.save(temp_path, "JPEG", quality=initial_quality)
 
